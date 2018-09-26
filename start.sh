0d49b6644055affe7d52ab4c356ba231cf3990d9
RPSeq <ryan.smith.p@gmail.com>
2018-09-25 23:05:28 -0700

    Continued collapsing apt-get commands

--- start.sh
+++ start.sh
@@ -1,31 +1,38 @@
+#!/bin/bash
+
 # init as root
 su root
-# add non-free sources
-echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" \
-    >> /etc/apt/sources.list
 apt-get update
 apt-get install \
-    firmware-iwlwifi \
     git \
-    vim \
-    sudo \
-    curl \
-    libpam-fprintd \
-    fprint-demo
-
-# turn on wifi (without restart)
-modprobe -r iwlwifi ; modprobe iwlwifi
+    sudo
 
 # add ryan to sudo group
 usermod -aG sudo ryan
+
+# leave root
 exit
 
-# sublime
+# add non-free sources
+echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" | sudo teee /etc/apt/sources.list.d/non-free.list
+
+# add sublime gpg key and source
 sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-sudo echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
-sudo apt install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+
+# update sources and install
 sudo apt update
-sudo apt install sublime-text
+sudo apt install \ 
+    sublime-text \
+    firmware-iwlwifi \
+    vim \
+    curl \
+    libpam-fprintd \
+    fprint-demo \
+    apt-transport-https
+
+# turn on wifi (without restart)
+modprobe -r iwlwifi ; modprobe iwlwifi
 
 # keybase
 sudo curl -O https://prerelease.keybase.io/keybase_amd64.deb
