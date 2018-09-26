e701ea15ab303bf3cbccb4544f1b65d56383b1bd
RPSeq <ryan.smith.p@gmail.com>
2018-09-25 22:10:03 -0700

    Tidy up start script

--- start.sh
+++ start.sh
@@ -1,24 +1,34 @@
+# init as root
 su root
-
+# add non-free sources
 echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" \
     >> /etc/apt/sources.list
+apt-get update
+apt-get install \
+    firmware-iwlwifi \
+    git \
+    vim \
+    sudo \
+    curl \
+    libpam-fprintd \
+    fprint-demo
 
-apt-get update && apt-get install firmware-iwlwifi
+# turn on wifi (without restart)
 modprobe -r iwlwifi ; modprobe iwlwifi
-apt-get install git vim sudo curl
 
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
-apt install apt-transport-https
-apt update
-apt install sublime-text
-
-apt install libpam-fprintd fprint-demo
-
+# add ryan to sudo group
+usermod -aG sudo ryan
 exit
 
-curl -O https://prerelease.keybase.io/keybase_amd64.deb
+# sublime
+sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo echo "deb https://download.sublimetext.com/ apt/stable/" > /etc/apt/sources.list.d/sublime-text.list
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install sublime-text
+
+# keybase
+sudo curl -O https://prerelease.keybase.io/keybase_amd64.deb
 sudo dpkg -i keybase_amd64.deb
 sudo apt-get install -f
 run_keybase
-
