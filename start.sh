074e38d51680d8789aab8b64ce7874e11ef1b1d7
RPSeq <ryan.smith.p@gmail.com>
2018-09-26 17:37:28 -0700

    Organize into functions and add firefox

--- start.sh
+++ start.sh
@@ -3,48 +3,83 @@
 # netinstall (with nonfree firmware)
 # https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/9.5.0+nonfree/amd64/iso-cd/firmware-9.5.0-amd64-netinst.iso
 
+set -euxo pipefail
+
 # init as root
-su root
-apt-get update
-apt-get install \
-    apt-transport-https \
-    git \
-    curl \
-    sudo
+init() {
+    su root                 # as root
+    apt-get update
+    apt-get install \
+        apt-transport-https \
+        git \
+        sudo \
+        lsb-release
+    usermod -aG sudo ryan   # add ryan to sudo group
+    exit                    # leave root
+}
 
-# add ryan to sudo group
-usermod -aG sudo ryan
-
-# leave root
-exit
 
 # configure git
-git config --global user.name "RPSeq"
-git config --global user.email "ryan.smith.p@gmail.com"
+git() {
+    git config --global user.name "RPSeq"
+    git config --global user.email "ryan.smith.p@gmail.com"
+}
 
-# add non-free sources
-echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" | sudo tee /etc/apt/sources.list.d/non-free.list
+# add sources
+sources() {
+    echo "deb http://httpredir.debian.org/debian/ stretch main contrib non-free" | sudo tee /etc/apt/sources.list.d/non-free.list
+    echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+    echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+}
 
-# add sublime gpg key and source
-sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+# add apt keys
+keys() {
+    sudo wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+    sudo wget -qO - https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+}
 
 # update sources and install
-sudo apt update
-sudo apt install \
-    vim-gnome \
-    sublime-text \
-    firmware-iwlwifi \
-    libpam-fprintd \
-    fprint-demo \
-    editorconfig \
-    htop
+apt() {
+    sudo apt-get update
+    sudo apt-get install \
+        vim-gnome \
+        sublime-text \
+        libpam-fprintd \
+        fprint-demo \
+        editorconfig \
+        curl \
+        ca-certificates \
+        gnupg2 \
+        software-properties-common \
+        htop \
+        docker-ce \
+        python \
+        python-pip \
+        xclip
+}
 
-# turn on wifi (without restart)
-modprobe -r iwlwifi ; modprobe iwlwifi
+# manual installs
+# firefox
+firefox() {
+    curl -fsSL -o firefox.tar.bz2 "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US"
+    sudo tar -C /opt -xvf firefox.tar.bz2
+    sudo rm -rf firefox.tar.bz2
+    sudo ln -sf "$(pwd)"/firefox.desktop /usr/share/applications/firefox.desktop
+    sudo apt-get remove firefox-esr
+}
 
 # keybase
-sudo curl -O https://prerelease.keybase.io/keybase_amd64.deb
-sudo dpkg -i keybase_amd64.deb
-sudo apt-get install -f
-run_keybase
+keybase() {
+    sudo curl -O https://prerelease.keybase.io/keybase_amd64.deb
+    sudo dpkg -i keybase_amd64.deb
+    sudo apt-get install -f
+    run_keybase
+}
+
+init
+git
+sources
+keys
+apt
+firefox
+keybase
