adfbb94f2b38103ddcfa8c64d4ee0c9b2ec5ec65
Ryan Smith <ryan.smith@eero.com>
2019-04-18 13:38:01 -0700

    Add docker and docker-compose

--- start.sh
+++ start.sh
@@ -47,6 +47,30 @@ scala() {
     sudo apt-get install sbt
 }
 
+docker() {
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get -y update
+    sudo apt-get -y install \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io
+}
+
+compose() {
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    sudo curl -L https://raw.githubusercontent.com/docker/compose/1.24.0/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+}
+
+kube() {
+    sudo apt-get update && sudo apt-get install -y apt-transport-https
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+    echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+    sudo apt-get update
+    sudo apt-get install -y kubectl
+}
+
 config() {
     ln -sf ${DIR}/editor/.editorconfig ~/.editorconfig
     ln -sf ${DIR}/vim/.vimrc ~/.vimrc
@@ -59,9 +83,12 @@ rem() {
     sudo apt-get autoremove -y
 }
 
-#install
-#vundle
-#discord
+install
+vundle
+discord
 scala
-#config
-#rem
+docker
+compose
+kube
+config
+rem
